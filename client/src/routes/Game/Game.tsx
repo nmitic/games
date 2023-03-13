c0a6b7894bdabd69d07212d8ab1f2b6f8789dc2a
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-03-13 10:33:10 +0100

    Added more content and styles to game route

--- client/src/routes/Game/Game.tsx
+++ client/src/routes/Game/Game.tsx
@@ -1,12 +1,25 @@
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
+import Typography from "../../ui-lib-components/Typography";
+import {allowedColors, allowedVariants} from "../../ui-lib-components/Typography/Typography";
+import style from './Game.module.css'
+
 export const Game = () => {
     const location = useLocation()
     const data = location.state?.data
 
+    const screenshotUrl = `http://www.royalgames.com/images/games/${data.short}/dumps/screen_${data.short}.gif`
+    const screenshotAltText = `screenshoot of the game called ${data.name}`
+
     return (
         <article>
-            <h2>{data.name}</h2>
+            <Typography color={allowedColors.gold}>{data.name}</Typography>
 
+            <Typography variant={allowedVariants.h3}>Screen shoot:</Typography>
+            <img src={screenshotUrl} alt={screenshotAltText} />
+            <Typography variant={allowedVariants.h3}>Game's meta data:</Typography>
+            <pre className={style.metaData}>
+                {`${JSON.stringify(data, null, 2)}`}
+            </pre>
         </article>
     )
 }
