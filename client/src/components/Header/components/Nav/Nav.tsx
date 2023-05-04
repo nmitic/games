1d90a3e2117d8642edf3d96ab84352b94deda833
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-04 10:57:43 +0200

    Revert "updated games route path"
    This reverts commit b974e71d237e9f078026624f1306445beecc9ed9.

--- client/src/components/Header/components/Nav/Nav.tsx
+++ client/src/components/Header/components/Nav/Nav.tsx
@@ -1,99 +1,76 @@
-import React, { useEffect } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import Icon, { allowedIcons } from "ui-lib-components/Icon";
-import style from "./Nav.module.css";
-import Button from "../../../../ui-lib-components/Button";
-import { createPortal } from "react-dom";
-import {
-  allowedColor,
-  allowedSizes,
-} from "../../../../ui-lib-components/Icon/Icon";
+import React, {useEffect} from "react"
+import {NavLink, useLocation} from "react-router-dom"
+import Icon, {allowedIcons} from "ui-lib-components/Icon"
+import style from './Nav.module.css'
+import Button from "../../../../ui-lib-components/Button"
+import {createPortal} from "react-dom"
+import {allowedColor, allowedSizes} from "../../../../ui-lib-components/Icon/Icon"
 
 type NavProps = {
-  isMenuOpen: Boolean;
-  closeMenu: () => void;
-};
+    isMenuOpen: Boolean,
+    closeMenu: () => void,
+}
 
-export const Nav: React.FC<NavProps> = ({ isMenuOpen, closeMenu }) => {
-  const location = useLocation();
-  const classNames = `${style.nav} ${isMenuOpen ? style.navOpen : ""}`;
-  const backdropClassNames = `${style.navBackdrop} ${
-    isMenuOpen ? style.navBackdropOpen : ""
-  }`;
-  const handleKeyDown = (e: KeyboardEvent) => {
-    if (e.key === "Escape") {
-      closeMenu();
+export const Nav:React.FC<NavProps> = ({isMenuOpen, closeMenu}) => {
+    const location = useLocation()
+    const classNames = `${style.nav} ${isMenuOpen ? style.navOpen : ''}`
+    const backdropClassNames = `${style.navBackdrop} ${isMenuOpen ? style.navBackdropOpen : ''}`
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            closeMenu()
+        }
     }
-  };
 
-  useEffect(() => {
-    closeMenu();
-  }, [location]);
+    useEffect(() => {
+        closeMenu()
+    }, [location])
 
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyDown)
 
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown)
+        }
+    }, [])
 
-  return (
-    <nav className={classNames}>
-      <Button onClick={closeMenu}>
-        <Icon iconName={allowedIcons.close} color={allowedColor.gold} />
-      </Button>
-      <ul className={style.navList}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? style.navLinkActive : "")}
-          >
-            <div className={`${style.navItem}`}>
-              <div className={style.navItemUpperPart} data-name="Home"></div>
-              <div className={style.navItemLowerPart} data-name="Home"></div>
-            </div>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="games-list"
-            className={({ isActive }) => (isActive ? style.navLinkActive : "")}
-          >
-            <div className={style.navItem}>
-              <div
-                className={style.navItemUpperPart}
-                data-name="Games Library"
-              ></div>
-              <div
-                className={style.navItemLowerPart}
-                data-name="Games Library"
-              ></div>
-            </div>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="portfolio"
-            className={({ isActive }) => (isActive ? style.navLinkActive : "")}
-          >
-            <div className={style.navItem}>
-              <div
-                className={style.navItemUpperPart}
-                data-name="Games Portfolio"
-              ></div>
-              <div
-                className={style.navItemLowerPart}
-                data-name="Games Portfolio"
-              ></div>
-            </div>
-          </NavLink>
-        </li>
-      </ul>
-      {createPortal(
-        <div className={backdropClassNames} onClick={closeMenu} />,
-        document.body
-      )}
-    </nav>
-  );
-};
+    return (
+        <nav className={classNames}>
+            <Button onClick={closeMenu} >
+                <Icon
+                    iconName={allowedIcons.close}
+                    color={allowedColor.gold}
+                />
+            </Button>
+            <ul className={style.navList}>
+                <li>
+                    <NavLink to="/" className={({isActive}) => isActive ? style.navLinkActive : ''}>
+                        <div className={`${style.navItem}`}>
+                            <div className={style.navItemUpperPart} data-name="Home"></div>
+                            <div className={style.navItemLowerPart} data-name="Home"></div>
+                        </div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="games" className={({isActive}) => isActive ? style.navLinkActive : ''}>
+                        <div className={style.navItem}>
+                            <div className={style.navItemUpperPart} data-name="Games Library"></div>
+                            <div className={style.navItemLowerPart} data-name="Games Library"></div>
+                        </div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="portfolio" className={({isActive}) => isActive ? style.navLinkActive : ''}>
+                        <div className={style.navItem}>
+                            <div className={style.navItemUpperPart} data-name="Games Portfolio"></div>
+                            <div className={style.navItemLowerPart} data-name="Games Portfolio"></div>
+                        </div>
+                    </NavLink>
+                </li>
+            </ul>
+            {createPortal(
+                <div className={backdropClassNames} onClick={closeMenu}/>,
+                document.body
+            )}
+        </nav>
+    )
+}
--- client/src/routes/Root/Root.tsx
+++ client/src/routes/Root/Root.tsx
@@ -1,157 +1,138 @@
-import {
-  Link,
-  Outlet,
-  useLoaderData,
-  useLocation,
-  useOutletContext,
-} from "react-router-dom";
-import React, { useState } from "react";
-import { Layout } from "./components/Layout/Layout";
-import { Header } from "components/Header/Header";
-import { Footer } from "../../components/Footer/Footer";
-import { ButtonCoverEffect } from "../../ui-lib-components/Button";
-import { allowedCoverEffectFrom } from "../../ui-lib-components/Button/Button";
-import style from "./Root.module.css";
+import {Link, Outlet, useLoaderData, useLocation, useOutletContext} from 'react-router-dom'
+import React, {useState} from "react";
+import {Layout} from "./components/Layout/Layout";
+import {Header} from "components/Header/Header";
+import {Footer} from "../../components/Footer/Footer";
+import {ButtonCoverEffect} from "../../ui-lib-components/Button";
+import {allowedCoverEffectFrom} from "../../ui-lib-components/Button/Button";
+import style from './Root.module.css';
 import Typography from "../../ui-lib-components/Typography";
-import {
-  allowedHtmlTag,
-  allowedVariants,
-} from "../../ui-lib-components/Typography/Typography";
+import {allowedHtmlTag, allowedVariants} from "../../ui-lib-components/Typography/Typography";
 
 export type gameDataType = {
-  name: string;
-  short: string;
-  url: string;
-  tags: string;
-  hasBoosters: boolean;
-  liked?: boolean;
-};
+    name: string,
+    short: string,
+    url: string,
+    tags: string,
+    hasBoosters: boolean,
+    liked?: boolean
+}
 
-export type gamesDataType = gameDataType[];
+export type gamesDataType = gameDataType[]
 
 type GamesContextType = {
-  games: gamesDataType;
-  addPortfolioGames: (gameToAdd: gameDataType) => gamesDataType;
-  removePortfolioGames: (gameToRemove: gameDataType) => gamesDataType;
-  searchGames: (dataSet: gamesDataType, searchTerm: string) => gamesDataType;
-  portfolioGames: gamesDataType;
+    games: gamesDataType,
+    addPortfolioGames: (gameToAdd: gameDataType) => gamesDataType,
+    removePortfolioGames: (gameToRemove: gameDataType) => gamesDataType,
+    searchGames: (dataSet: gamesDataType, searchTerm: string) => gamesDataType,
+    portfolioGames : gamesDataType
 };
 
-export const useGames = () => useOutletContext<GamesContextType>();
+export const useGames = () => useOutletContext<GamesContextType>()
 
 export const loader = async (): Promise<{ games: gamesDataType }> => {
-  const cachedGamesData = readDataFromBrowserStorage("games");
+    const cachedGamesData = readDataFromBrowserStorage('games')
 
-  if (cachedGamesData) {
-    return cachedGamesData;
-  }
+    if (cachedGamesData) {
+        return cachedGamesData
+    }
 
-  const response = await fetch("http://localhost:12345/games.json");
-  const { games } = await response.json();
+    const response = await fetch('http://localhost:12345/games.json')
+    const {games} = await response.json()
 
-  const gamesState = games.map((game: gamesDataType) => ({
-    ...game,
-    liked: false,
-  }));
+    const gamesState = games.map((game: gamesDataType) => ({...game, liked: false}))
 
-  persistDataIntoBrowserStorage(gamesState, "games");
+    persistDataIntoBrowserStorage(gamesState, 'games')
 
-  return gamesState;
-};
+    return gamesState
+}
 
 const persistDataIntoBrowserStorage = (data: {}, key: string) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
+    localStorage.setItem(key, JSON.stringify(data));
+}
 
 const readDataFromBrowserStorage = (key: string) => {
-  try {
-    const data = localStorage.getItem(key);
-    if (data) {
-      return JSON.parse(data);
+    try {
+        const data = localStorage.getItem(key)
+        if (data) {
+            return JSON.parse(data)
+        }
+    } catch (e) {
+        console.log(e)
     }
-  } catch (e) {
-    console.log(e);
-  }
-};
+}
 
 export const Root = () => {
-  const isNotRootRoute = useLocation().pathname !== "/";
-  const gamesData = useLoaderData() as gamesDataType;
-  const [games, setGames] = useState<gamesDataType>(gamesData);
+    const isNotRootRoute = useLocation().pathname !== '/'
+    const gamesData = useLoaderData() as gamesDataType
+    const [games, setGames] = useState<gamesDataType>(gamesData)
 
-  const handleSetGames = (games: gamesDataType) => {
-    setGames(games);
-    persistDataIntoBrowserStorage(games, "games");
-  };
+    const handleSetGames = (games: gamesDataType) => {
+        setGames(games)
+        persistDataIntoBrowserStorage(games, 'games')
+    }
 
-  const addPortfolioGames = (gameToAdd: gameDataType) => {
-    const gamesAfterAdding = games.map((game) => {
-      if (game.name === gameToAdd.name) {
-        game.liked = true;
-      }
+    const addPortfolioGames = (gameToAdd: gameDataType) => {
+        const gamesAfterAdding = games.map((game) => {
+            if (game.name === gameToAdd.name) {
+                game.liked = true
+            }
 
-      return game;
-    });
+            return game
+        })
 
-    handleSetGames(gamesAfterAdding);
-  };
+        handleSetGames(gamesAfterAdding)
+    }
 
-  const removePortfolioGames = (gameToAdd: gameDataType) => {
-    const gamesAfterRemoval = games.map((game) => {
-      if (game.name === gameToAdd.name) {
-        game.liked = false;
-      }
+    const removePortfolioGames = (gameToAdd: gameDataType) => {
+        const gamesAfterRemoval = games.map((game) => {
+            if (game.name === gameToAdd.name) {
+                game.liked = false
+            }
 
-      return game;
-    });
-    handleSetGames(gamesAfterRemoval);
-  };
+            return game
+        })
+        handleSetGames(gamesAfterRemoval)
+    }
 
-  const searchGames = (dataSet: gamesDataType, searchTerm: string) => {
-    return dataSet.filter((game: gameDataType) =>
-      game.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  };
+    const searchGames = (dataSet: gamesDataType, searchTerm: string) => {
+        return dataSet.filter((game:gameDataType) => game.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    }
 
-  const portfolioGames = games.filter((game) => game.liked);
+    const portfolioGames = games.filter(game => game.liked)
 
-  return (
-    <Layout
-      header={<Header />}
-      main={
-        isNotRootRoute ? (
-          <Outlet
-            context={{
-              portfolioGames,
-              games,
-              addPortfolioGames,
-              removePortfolioGames,
-              searchGames,
-            }}
-          />
-        ) : (
-          <div className={style.root}>
-            <Typography
-              component={allowedHtmlTag.h1}
-              variant={allowedVariants.h4}
-            >
-              Welcome to the most awesome but totally useless website where you
-              can not really do much, just click here and there, but hey look at
-              the navigation menu cool effects right? And how about this cool
-              button hover effect?
-            </Typography>
-            <Link to="games-list">
-              <ButtonCoverEffect
-                coverEffectFrom={allowedCoverEffectFrom.right}
-                className={style.exploreNowBtn}
-              >
-                Explore Games Now
-              </ButtonCoverEffect>
-            </Link>
-          </div>
-        )
-      }
-      footer={<Footer />}
-    />
-  );
-};
+    return (
+        <Layout
+            header={<Header/>}
+            main={isNotRootRoute ? <Outlet context={
+                {
+                    portfolioGames,
+                    games,
+                    addPortfolioGames,
+                    removePortfolioGames,
+                    searchGames
+                }}/> : (
+                <div className={style.root}>
+                    <Typography
+                        component={allowedHtmlTag.h1}
+                        variant={allowedVariants.h4}
+                    >
+                        Welcome to the most awesome but totally useless website where you can not really do much, just
+                        click here and there,
+                        but hey look at the navigation menu cool effects right? And how about this cool button hover
+                        effect?
+                    </Typography>
+                    <Link to="games">
+                        <ButtonCoverEffect
+                            coverEffectFrom={allowedCoverEffectFrom.right}
+                            className={style.exploreNowBtn}
+                        >
+                            Explore Games Now
+                        </ButtonCoverEffect>
+                    </Link>
+                </div>
+            )}
+            footer={<Footer/>}
+        />
+    )
+}
