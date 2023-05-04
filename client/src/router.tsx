b974e71d237e9f078026624f1306445beecc9ed9
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-04 10:50:16 +0200

    updated games route path

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,27 +1,27 @@
-import {createBrowserRouter} from "react-router-dom";
-import {Root, loader as rootLoader} from "./routes/Root/Root";
-import {Portfolio} from "./routes/Portfolio/Portfolio";
-import {Game} from "./routes/Game/Game";
-import {Games} from "./routes/Games/Games";
+import { createBrowserRouter } from "react-router-dom";
+import { Root, loader as rootLoader } from "./routes/Root/Root";
+import { Portfolio } from "./routes/Portfolio/Portfolio";
+import { Game } from "./routes/Game/Game";
+import { Games } from "./routes/Games/Games";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        loader: rootLoader,
-        children: [
-            {
-                path: "/games",
-                element: <Games />,
-            },
-            {
-                path: "/games/:gameId",
-                element: <Game />
-            },
-            {
-                path: "/portfolio",
-                element: <Portfolio />
-            },
-        ]
-    }
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "/games-list",
+        element: <Games />,
+      },
+      {
+        path: "/games-list/:gameId",
+        element: <Game />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+    ],
+  },
 ]);
